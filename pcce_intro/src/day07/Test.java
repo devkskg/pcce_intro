dfcf661b53b57da63e3fd8fa8f6fc138d10bc1b2
study <study@DESKTOP-KJH1RBD>
2024-12-15 21:57:30 +0900

    [ksk] 형변환 테스트

---
+++ pcce_intro/src/day07/Test.java
@@ -0,0 +1,19 @@
+package day07;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		int num = 100;
+		String numToStr = Integer.toString(num);
+		System.out.println(numToStr);
+		
+		String str = "rrr";
+		System.out.println(numToStr + str);
+		
+		
+
+	}
+
+}
