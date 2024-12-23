dfb07f08c2e17b51614af4cc256692b4830ab5b4
devkskg <devkskg@gmail.com>
2024-12-23 16:34:32 +0900

    [level 0] Title: 암호 해독, Time: 24.17 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for(int i = 0; i < cipher.length(); i++){
+            if(i % code == code - 1){
+            answer += (char)cipher.charAt(i);
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
