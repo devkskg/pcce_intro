5299f598044e834cbccf2fa6ba93ae11369a9dd7
devkskg <devkskg@gmail.com>
2024-12-23 14:17:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int sum = 0;
+        for(int i = 0; i < my_string.length(); i++){
+            if('0' <= my_string.charAt(i) && my_string.charAt(i) <= '9'){
+                sum += my_string.charAt(i) - '0';
+            }
+        }
+        
+        return sum;
+    }
+}
