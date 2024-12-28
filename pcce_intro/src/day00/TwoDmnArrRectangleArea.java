57fcaa54785a04fe20b1420a46bba7d7fba31513
study <study@DESKTOP-KJH1RBD>
2024-12-28 20:02:48 +0900

    [ksk] TwoDmnArrRectangleArea
    	이중배열 감잡기

---
+++ pcce_intro/src/day00/TwoDmnArrRectangleArea.java
@@ -0,0 +1,12 @@
+package day00;
+
+public class TwoDmnArrRectangleArea {
+
+	public static void main(String[] args) {
+//		int[][] dots = {{},{},{},{}};
+		int[][] dots = {{1,1},{2,1},{2,2},{1,2}};
+		System.out.println(dots[1][0]);
+//		이중배열은 하나씩 아래로 쌓는다고 생각하면 편했잖아
+	}
+
+}
