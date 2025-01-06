bc0295c46ee7bb7875cc032f2188ce07faae02f9
devkskg <devkskg@gmail.com>
2025-01-06 21:35:11 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.30 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        String[] temp = my_string.split(" ");
+        List<String> list = new ArrayList<String>();
+        
+        for(int i = 0; i < temp.length; i++){
+            if(temp[i].equals("")){
+                continue;
+            } else{
+                list.add(temp[i]);
+            }
+        }
+        
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
