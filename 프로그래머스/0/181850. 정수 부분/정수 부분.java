54ada7d844e43f7e278285a3c56f5d38e0ead215
devkskg <devkskg@gmail.com>
2025-01-06 17:47:57 +0900

    [level 0] Title: 정수 부분, Time: 0.31 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        double temp = Math.floor(flo);
+        answer = (int)temp;
+        return (int)answer;
+    }
+}
